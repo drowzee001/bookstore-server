c68028362b0ebcb8d4c8d9cb4b51729ac955ef3d
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-26 19:11:12 -0700

    Fixed Port Typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 const db = process.env.MONGO_URI;
 
